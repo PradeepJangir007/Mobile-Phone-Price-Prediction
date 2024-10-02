b0cbc7dff6eb0984bea7289ada4485b57bb9dc45
PradeepJangir077 <pradeepjangir.ml007@gmail.com>
2024-10-02 14:52:37 +0530

    train new model

--- app.py
+++ app.py
@@ -2,11 +2,14 @@ import streamlit as st
 import pandas as pd
 import pickle
 import sys
-from my_module import SparseToDenseTransformer
+
+import my_module
+from my_module import SparseToDenseTransformer,LogStandardScaler,confidence_
 sys.modules['__main__'].SparseToDenseTransformer = SparseToDenseTransformer
+sys.modules['__main__'].confidence_ = confidence_
 
 df=pickle.load(open('df.pkl','rb'))
-model=pickle.load(open('smart_phone_price_Lasso.pkl','rb'))
+model=pickle.load(open('smart_phone_price_with_PLSR.pkl','rb'))
 st.title('smart_phone_price_prediction')
 st.subheader('give some information about smart phone')
 st.markdown('Note: yes = 1 ,No = 0')
@@ -42,16 +45,18 @@ if b==True:
     h=int(str(disply).split('*')[1])
     pixal_D=(v**2+h**2)**0.5
     PPI=((v**2+h**2)/float(size))**0.5
-    x={
-        'Model_series':[model_series],'Dual Sim':[int(Dual_sim)],'VoLTE':[int(VoLTE)],'5G': [int(G)],
+    Processor_series=my_module.Processor_s.Transformer(Processor,Processor_series)
+    x={'Model_series':[model_series],'Dual Sim':[int(Dual_sim)],'VoLTE':[int(VoLTE)],'5G': [int(G)],
         'Vo5G':[int(Vo5G)],'Ram':[int(Ram)],'Battery':[int(Battery)],'Foldable Display':[int(Foldable_Display)],
         'Dual Display':[int(Dual_Displayd)],'External_Memory':[int(External_Memory)],
         'company':[Brand],'Inbuilt_memory':[int(Inbuilt_memory)],'fast_charging':[float(fast_charging)],
         'Processor_':[Processor],'Processor_series':[int(Processor_series)],'No _of_Rear':No_of_Rear,'No _of_Front':[int(No_of_Front)],
         'Primary_rear_camera':[Primary_rear_camera],'Primary_front_camera':[Primary_front_camera],
         'Number_of_core':[Number_of_core],
-        'PPI':[PPI],
-    }
+        'PPI':[PPI]}
     x=pd.DataFrame(x)
-    print(x)
     st.title('price ' +str(round(2.7183**((model.predict(x))[0]))))
+    conf=confidence_()
+    L=conf.interval(x)[0]
+    U=conf.interval(x)[1]
+    st.subheader(f'confidence interval with 90% \n {(round(2.7183 ** L), round(2.7183 ** U))}')
--- my_module.py
+++ my_module.py
@@ -1,5 +1,50 @@
+from sklearn.base import TransformerMixin, BaseEstimator
+from sklearn.preprocessing import StandardScaler
+import numpy as np
+import pickle
 class SparseToDenseTransformer():
     def fit(self, X, y=None):
         return self
     def transform(self, X, y=None):
         return X.toarray()
+class Processor_s():
+    def Transformer(x,y):
+        y=float(y)
+        if x=='snapdragon':
+            if (y) < 10:
+                return round(round(y)*115+(y-round(y))*500)
+            elif y>4000:
+                return(y/9)
+            else :
+                return y
+        else:
+            return y
+class LogStandardScaler(TransformerMixin,BaseEstimator):
+    def __init__(self):
+        self.scaler = StandardScaler()
+
+    def fit(self, y):
+        y_log = np.log1p(y)
+        self.scaler.fit(y_log)
+        return self
+
+    def transform(self, y):
+        y_log = np.log1p(y)
+        return self.scaler.transform(y_log)
+
+    def inverse_transform(self, y):
+        y_scaled = self.scaler.inverse_transform(y)
+        return np.expm1(y_scaled)
+class confidence_():
+    def __init__(self):
+        self.model=pickle.load((open('smart_phone_price_with_PLSR.pkl','rb')))
+        self.pipe=pickle.load(open('pipe.pkl','rb'))
+        self.pls_x=self.model.named_steps['reg'].regressor_.x_scores_
+        self.x_weights_=self.model.named_steps['reg'].regressor_.x_weights_
+    def interval(self,x):
+        Y=self.model.predict(x)
+        XTX_inv=np.linalg.inv(self.pls_x.T.dot(self.pls_x))
+        X_conponet=self.pipe.transform(x).dot(self.x_weights_).reshape(-1,1)
+        lower=Y-1.6*np.sqrt(((X_conponet.T.dot(XTX_inv)).dot(X_conponet)+1)*0.0229)
+        upper=Y+1.6*np.sqrt(((X_conponet.T.dot(XTX_inv)).dot(X_conponet)+1)*0.0229)
+        return (lower[0,0],upper[0,0])
